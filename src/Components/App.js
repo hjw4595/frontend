09bb0e20820e5f1e440fd7f139477efe5aa2913b
hjw4595 <hjw4595@naver.com>
2020-04-12 02:07:05 +0900

    grobal style -Theme

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+export default () => (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+)
---
+++ src/Styles/Theme.js
@@ -0,0 +1,17 @@
+const BOX_BODER = "1px solid #e6e6e6" ;
+const BORDER_RADIUS = "4px";
+export default {
+    bgColor : "#FAFAFA",
+    blackColor:"#262626",
+    blueColor:"3897f0",
+    darkGreyColor:"#999",
+    lightGreyColor:"#c7c7c7",
+    redColor: "#ED4956",
+    darkColor:"#003569",
+    borderRadius: "4px",
+    whiteBox: `
+    ${BORDER_RADIUS};
+    ${BOX_BODER};
+    background-color:white;
+    `
+}
